e5ac437758f6d62bef7831d00fd0e3b6262c7417
LordxMaanit <75978896+LordxMaanit@users.noreply.github.com>
2021-05-10 14:54:21 +0530

    Add files via upload

---
+++ SlingShot.js
@@ -0,0 +1,35 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 5
+        }
+
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+    attach(Body){
+        this.sling.bodyA = body ;
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    display(){
+
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            
+            stroke("red");
+            strokeWeight(4);
+            line(pointA.x ,pointA.y, pointB.x , pointB.y);
+                
+        }
+    }
+    
+}
